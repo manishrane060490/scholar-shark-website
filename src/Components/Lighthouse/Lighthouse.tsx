fcd44b5f367844b042743a98efd218c11f3d7439
Manish Rane <118064386+manishrane060490@users.noreply.github.com>
2024-03-25 18:22:14 +0530

    Update Lighthouse.tsx

--- src/Components/Lighthouse/Lighthouse.tsx
+++ src/Components/Lighthouse/Lighthouse.tsx
@@ -3,7 +3,148 @@ import React from 'react'
 function Lighthouse() {
   return (
     <div>
-      <h1>LightHouse</h1>
+      
+<div class="scene">
+  <div class="background">
+    <div class="stars">
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+    </div>
+    <div class="moon"></div>
+    <div class="mountains">
+      <div class="mountain"></div>
+      <div class="mountain"></div>
+      <div class="mountain"></div>
+      <div class="mountain"></div>
+    </div>
+    <div class="sea">
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="wave"></div>
+      <div class="waves">
+        <div class="boat">
+          <div class="base"></div>
+          <div class="sail"></div>
+          <div class="sail"></div>
+          <div class="sail"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="lighthouse-group">
+    <div class="land"></div>
+    <div class="lighthouse-holder">
+      <div class="shadow"></div>
+      <div class="lighthouse"></div>
+      <div class="top"></div>
+      <div class="windows">
+        <div class="window"></div>
+        <div class="window"></div>
+        <div class="window"></div>
+        <div class="window"></div>
+      </div>
+      <div class="door">
+        <div class="stairs"></div>
+      </div>
+      <div class="top">
+        <div class="light-container">
+          <div class="light"></div>
+        </div>
+        <div class="rail"></div>
+        <div class="middle"></div>
+        <div class="roof">
+          <div class="roof-light"></div>
+        </div>
+        <div class="glow"></div>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
