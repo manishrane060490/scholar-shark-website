51a65c8d4cd8d338ffe198d1743052df801f4836
Manish Rane <118064386+manishrane060490@users.noreply.github.com>
2024-03-25 17:19:51 +0530

    Update Lighthouse.tsx

--- src/Components/Lighthouse/Lighthouse.tsx
+++ src/Components/Lighthouse/Lighthouse.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+import lighthouse from '../../assets/images/Lighthouse.svg'
 
 function Lighthouse() {
   return (
-    <div>
-      <h1>LightHouse</h1>
+    <div className='lighthouse'>
+        <div className='sky'>
+        
+        </div>
+        <div className='water'></div>
+        <div className='stones'>
+            <div>
+            <img src={lighthouse} className='light-img'/>
+            <div className="light-container">
+            <div className="light"></div>
+            </div>
+            </div>
+            
+        </div>
     </div>
   )
 }
