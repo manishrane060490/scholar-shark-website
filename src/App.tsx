4c6592454e06c86052568be000e07fadcfac60db
Manish Rane <manish.rane6490@gmail.com>
2024-04-24 20:04:36 +0530

    initial code of video page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Videopage from './Components/Videopage/Videopage';
 import Language from './Pages/Language';
 
 function App() {
   return (
     <div className="App">
-      <Language />
+      {/* <Language /> */}
+      <Videopage />
     </div>
   );
 }
