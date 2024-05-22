2a57365e0db2083f455dfc5b6762f2a65fa65e68
Manish Rane <manish.rane6490@gmail.com>
2024-05-22 13:10:03 +0530

    quiz
    quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Quizpage from './Components/Quizpage/Quizpage';
 import Videopage from './Components/Videopage/Videopage';
 import Language from './Pages/Language';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <Language /> */}
-      <Videopage />
+      {/* <Videopage /> */}
+      <Quizpage />
     </div>
   );
 }
