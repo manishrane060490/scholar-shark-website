365debafc797fa82bd0858f9f29c4cf558560f72
Manish Rane <manish..mahadeorane@gmail.com>
2024-06-20 12:49:27 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             {/* <Route path="/" element={<Language />}> */}
-              <Route index element={<Quizpage />} />
+              <Route index element={<LanguagePage />} />
               <Route path="plans" element={<PlansPage />} />
               <Route path="quiz" element={<Quizpage />} />
               <Route path="register" element={<RegisterPage />} />
